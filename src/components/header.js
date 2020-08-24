5bec9b3222c89ee8598aa847fb4aa412378483e2
Brady Bridges <bradyjbridges@gmail.com>
2020-08-24 13:48:23 -0500

    Implement css modules to bring in basic styling

--- src/components/header.js
+++ src/components/header.js
@@ -1,56 +1,50 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
-import logo from "../images/logo.svg";
+import logo from "../images/logo.svg"
+import headerStyles from "./header.module.css"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
+  <header className={headerStyles.header}>
+    <div className={headerStyles.headerContainer}>
       <h1 style={{ margin: 0 }}>
         <Link
           to="/"
           style={{
             color: `white`,
             textDecoration: `none`,
+            display: "flex",
           }}
         >
-          <img src={ logo } alt="Arts Matter" />
+          <img style={{ margin: 0 }} src={logo} alt="Arts Matter" />
         </Link>
       </h1>
       <nav>
-        <ul>
-          <li>
-            <Link to="/about">About</Link>
+        <ul className={headerStyles.navList}>
+          <li className={headerStyles.linkLi}>
+            <Link className={headerStyles.navLink} to="/about">
+              About
+            </Link>
           </li>
-        </ul>
-        <ul>
-          <li>
-            <Link to="/programs">Programs</Link>
+          <li className={headerStyles.linkLi}>
+            <Link className={headerStyles.navLink} to="/programs">
+              Programs
+            </Link>
           </li>
-        </ul>
-        <ul>
-          <li>
-            <Link to="/resources">Resources</Link>
+          <li className={headerStyles.linkLi}>
+            <Link className={headerStyles.navLink} to="/resources">
+              Resources
+            </Link>
           </li>
-        </ul>
-        <ul>
-          <li>
-            <Link to="/news">News</Link>
+          <li className={headerStyles.linkLi}>
+            <Link className={headerStyles.navLink} to="/news">
+              News
+            </Link>
           </li>
-        </ul>
-        <ul>
-          <li>
-            <Link to="/contact">Contact</Link>
+          <li className={headerStyles.linkLi}>
+            <Link className={headerStyles.navLink} to="/contact">
+              Contact
+            </Link>
           </li>
         </ul>
       </nav>
