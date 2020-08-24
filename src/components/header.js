b3c8d9434516b512643986a2080d9a90cefa9aa8
Brady Bridges <bradyjbridges@gmail.com>
2020-08-24 15:55:25 -0500

    Add functionality to handle animated header on scroll

--- src/components/header.js
+++ src/components/header.js
@@ -1,59 +1,79 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
-import React from "react"
+import React, { useEffect, useRef } from "react"
 import logo from "../images/logo.svg"
-import headerStyles from "./header.module.css"
+import "./header.css"
 
-const Header = ({ siteTitle }) => (
-  <header className={headerStyles.header}>
-    <div className={headerStyles.headerContainer}>
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-            display: "flex",
-          }}
-        >
-          <img style={{ margin: 0 }} src={logo} alt="Arts Matter" />
-        </Link>
-      </h1>
-      <nav>
-        <ul className={headerStyles.navList}>
-          <li className={headerStyles.linkLi}>
-            <Link className={headerStyles.navLink} to="/about">
-              About
-            </Link>
-          </li>
-          <li className={headerStyles.linkLi}>
-            <Link className={headerStyles.navLink} to="/programs">
-              Programs
-            </Link>
-          </li>
-          <li className={headerStyles.linkLi}>
-            <Link className={headerStyles.navLink} to="/resources">
-              Resources
-            </Link>
-          </li>
-          <li className={headerStyles.linkLi}>
-            <Link className={headerStyles.navLink} to="/news">
-              News
-            </Link>
-          </li>
-          <li className={headerStyles.linkLi}>
-            <Link className={headerStyles.navLink} to="/contact">
-              Contact
-            </Link>
-          </li>
-        </ul>
-      </nav>
-    </div>
-  </header>
-)
+const Header = ({ siteTitle, active }) => {
+  const backPanelRef = useRef()
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll, true)
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll, true)
+    }
+  })
+
+  const handleScroll = () => {
+    if (
+      document.body.scrollTop > 10 ||
+      document.documentElement.scrollTop > 10
+    ) {
+      backPanelRef.current.classList.remove("expandedPanel")
+      backPanelRef.current.classList.add("backPanel")
+    } else {
+      backPanelRef.current.classList.remove("backPanel")
+      backPanelRef.current.classList.add("expandedPanel")
+    }
+  }
+
+  return (
+    <header className="header">
+      <div ref={backPanelRef} className="expandedPanel primary"></div>
+      <div className="headerContainer">
+        <h1 style={{ margin: 0 }}>
+          <Link to="/" className="navLink">
+            <img style={{ margin: 0 }} src={logo} alt="Arts Matter" />
+          </Link>
+        </h1>
+        <nav>
+          <ul className="navList">
+            <li className="linkLi">
+              <Link className="navLink" to="/about">
+                About
+              </Link>
+            </li>
+            <li className="linkLi">
+              <Link className="navLink" to="/programs">
+                Programs
+              </Link>
+            </li>
+            <li className="linkLi">
+              <Link className="navLink" to="/resources">
+                Resources
+              </Link>
+            </li>
+            <li className="linkLi">
+              <Link className="navLink" to="/news">
+                News
+              </Link>
+            </li>
+            <li className="linkLi">
+              <Link className="navLink" to="/contact">
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  )
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
+  active: PropTypes.string,
 }
 
 Header.defaultProps = {
