b372d043bf9708ebfbd2c7f1de59588bd95a04c3
Brady Bridges <bradyjbridges@gmail.com>
2020-08-24 15:56:03 -0500

    Add placeholder text to allow viewing of header animation on scroll

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,14 +2,93 @@ import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import logo from "../images/logo.svg";
+import logo from "../images/logo.svg"
 
-console.log(logo)
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Homepage</h1>
-    <p>Welcome!</p>
+    <p>
+      Enim ex laboris commodo sint ullamco amet mollit quis sunt id eu mollit
+      adipisicing. Laboris incididunt qui sunt laborum id commodo reprehenderit
+      voluptate consequat sunt cupidatat incididunt. Aute commodo quis fugiat
+      proident ad ipsum Lorem non est quis sunt eiusmod. Fugiat non minim
+      reprehenderit voluptate. Nostrud proident labore nostrud incididunt aute
+      magna irure amet excepteur ex irure id non laborum. Esse cupidatat aliqua
+      est occaecat minim aute. Nostrud qui qui cillum consectetur ullamco dolore
+      ullamco reprehenderit laboris irure magna velit. Id dolor sit officia
+      Lorem. Eiusmod velit veniam do proident ut cupidatat non do labore dolor
+      cillum ea irure occaecat. Non ex voluptate elit nulla proident dolore
+      labore magna deserunt laboris est dolore aliquip consectetur. Sint ullamco
+      officia ea commodo nulla culpa proident. Dolor anim amet et cupidatat.
+      Labore sit aliqua elit nisi sit dolore ullamco in excepteur ipsum veniam
+      fugiat non. Irure aliqua magna sint commodo reprehenderit. Esse cupidatat
+      nisi labore labore laborum excepteur enim. Voluptate mollit est eu elit
+      laborum culpa. Irure aliqua magna excepteur aliqua esse dolore cillum id
+      amet laborum proident ad. Magna ullamco enim anim nostrud adipisicing
+      officia aute cupidatat Lorem nisi. Non amet Lorem voluptate fugiat
+      voluptate velit eu pariatur. Lorem reprehenderit et nisi adipisicing magna
+      non anim officia exercitation magna elit excepteur labore ullamco. Ad qui
+      aliqua pariatur enim ipsum dolore pariatur. Reprehenderit commodo
+      consectetur proident exercitation deserunt elit. Ullamco do nisi ex elit
+      labore fugiat et. Veniam eu reprehenderit enim voluptate aliquip veniam
+      consectetur sint exercitation nostrud proident ex. Dolore esse ex commodo
+      duis deserunt duis eiusmod quis labore minim. Anim in duis proident Lorem
+      excepteur sit fugiat. Fugiat proident dolore do non proident nostrud
+      adipisicing ipsum ad excepteur veniam. Consequat nostrud excepteur labore
+      voluptate irure nostrud nostrud nulla esse. Anim reprehenderit veniam
+      irure irure labore laborum nisi. Mollit aute anim occaecat ipsum
+      exercitation. Lorem occaecat veniam irure sunt cupidatat non in aute dolor
+      labore est. Magna labore laboris eu dolore minim non occaecat aute irure
+      non. Consequat laboris in pariatur magna ex ut ad fugiat mollit
+      consectetur Lorem anim. Laboris proident sint non magna duis. Ullamco et
+      proident tempor nulla minim velit Lorem sint pariatur est duis minim.
+      Fugiat voluptate sint nulla laborum aute eiusmod. Reprehenderit
+      adipisicing deserunt velit sint. Veniam nulla consequat sint ad. Est
+      exercitation laborum deserunt consequat. Laboris magna sint occaecat
+      consequat voluptate cillum in et nostrud sint ut in. Pariatur et enim
+      ullamco eu exercitation Lorem consequat esse laborum duis aute Lorem duis.
+      Exercitation excepteur voluptate eu adipisicing enim consectetur. Fugiat
+      dolor sit id adipisicing. Voluptate aliquip cupidatat ea incididunt anim
+      laborum excepteur Lorem cupidatat proident ipsum duis. In enim nostrud
+      proident ad magna irure ea cupidatat aliqua adipisicing aliquip voluptate
+      veniam occaecat. Non quis duis consequat id mollit deserunt est.
+      Reprehenderit sit sint proident tempor consequat non. Aliquip id fugiat
+      elit eiusmod labore ad voluptate. Ad aliquip exercitation minim fugiat.
+      Excepteur exercitation aute nostrud ad officia labore incididunt. Irure
+      aliqua magna excepteur aliqua esse dolore cillum id amet laborum proident
+      ad. Magna ullamco enim anim nostrud adipisicing officia aute cupidatat
+      Lorem nisi. Non amet Lorem voluptate fugiat voluptate velit eu pariatur.
+      Lorem reprehenderit et nisi adipisicing magna non anim officia
+      exercitation magna elit excepteur labore ullamco. Ad qui aliqua pariatur
+      enim ipsum dolore pariatur. Reprehenderit commodo consectetur proident
+      exercitation deserunt elit. Ullamco do nisi ex elit labore fugiat et.
+      Veniam eu reprehenderit enim voluptate aliquip veniam consectetur sint
+      exercitation nostrud proident ex. Dolore esse ex commodo duis deserunt
+      duis eiusmod quis labore minim. Anim in duis proident Lorem excepteur sit
+      fugiat. Fugiat proident dolore do non proident nostrud adipisicing ipsum
+      ad excepteur veniam. Consequat nostrud excepteur labore voluptate irure
+      nostrud nostrud nulla esse. Anim reprehenderit veniam irure irure labore
+      laborum nisi. Mollit aute anim occaecat ipsum exercitation. Lorem occaecat
+      veniam irure sunt cupidatat non in aute dolor labore est. Magna labore
+      laboris eu dolore minim non occaecat aute irure non. Consequat laboris in
+      pariatur magna ex ut ad fugiat mollit consectetur Lorem anim. Laboris
+      proident sint non magna duis. Ullamco et proident tempor nulla minim velit
+      Lorem sint pariatur est duis minim. Fugiat voluptate sint nulla laborum
+      aute eiusmod. Reprehenderit adipisicing deserunt velit sint. Veniam nulla
+      consequat sint ad. Est exercitation laborum deserunt consequat. Laboris
+      magna sint occaecat consequat voluptate cillum in et nostrud sint ut in.
+      Pariatur et enim ullamco eu exercitation Lorem consequat esse laborum duis
+      aute Lorem duis. Exercitation excepteur voluptate eu adipisicing enim
+      consectetur. Fugiat dolor sit id adipisicing. Voluptate aliquip cupidatat
+      ea incididunt anim laborum excepteur Lorem cupidatat proident ipsum duis.
+      In enim nostrud proident ad magna irure ea cupidatat aliqua adipisicing
+      aliquip voluptate veniam occaecat. Non quis duis consequat id mollit
+      deserunt est. Reprehenderit sit sint proident tempor consequat non.
+      Aliquip id fugiat elit eiusmod labore ad voluptate. Ad aliquip
+      exercitation minim fugiat. Excepteur exercitation aute nostrud ad officia
+      labore incididunt.
+    </p>
   </Layout>
 )
 
