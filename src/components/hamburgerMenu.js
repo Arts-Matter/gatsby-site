979b47c75fa466933ba732f577e7aac3c4e9b67a
Brady Bridges <bradyjbridges@gmail.com>
2020-08-25 12:20:58 -0500

    Add mobile menu

---
+++ src/components/hamburgerMenu.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react"
+import "./hamburgerMenu.css"
+
+export default function HamburgerMenu({
+  headerRef,
+  setMobileExpanded,
+  mobileNavRef,
+}) {
+  console.log(mobileNavRef)
+  const [active, setActive] = useState(false)
+  const inactiveClass = "hamburger hamburger--slider"
+  const activeClass = "hamburger hamburger--slider is-active"
+
+  const handleClick = () => {
+    setActive(!active)
+
+    if (!active) {
+      headerRef.current.classList.add("header--secondary")
+      headerRef.current.classList.remove("header--primary")
+      mobileNavRef.current.style.height = "50vh"
+      setMobileExpanded(true)
+    } else {
+      headerRef.current.classList.add("header--primary")
+      headerRef.current.classList.remove("header--secondary")
+      mobileNavRef.current.style.height = "0px"
+      setMobileExpanded(false)
+    }
+  }
+
+  return (
+    <button
+      className={active ? activeClass : inactiveClass}
+      type="button"
+      onClick={handleClick}
+    >
+      <span className="hamburger-box">
+        <span className="hamburger-inner"></span>
+      </span>
+    </button>
+  )
+}
