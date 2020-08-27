d6268cfe93a522cb0e9c68a4fb6e14a803258a8d
Brady Bridges <bradyjbridges@gmail.com>
2020-08-27 12:17:51 -0500

    Add listener and method to handle missing nav after going back to desktop

--- src/components/hamburgerMenu.js
+++ src/components/hamburgerMenu.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import "./hamburgerMenu.css"
 
 export default function HamburgerMenu({
@@ -9,6 +9,26 @@ export default function HamburgerMenu({
   const [active, setActive] = useState(false)
   const inactiveClass = "hamburger hamburger--slider"
   const activeClass = "hamburger hamburger--slider is-active"
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize)
+
+    return () => {
+      window.removeEventListener("resize", handleResize)
+    }
+  })
+
+  const handleResize = () => {
+    const width = document.documentElement.clientWidth
+
+    if (width > 889 && width < 900) {
+      setActive(false)
+      headerRef.current.classList.add("header--primary")
+      headerRef.current.classList.remove("header--secondary")
+      mobileNavRef.current.removeAttribute("style")
+      setMobileExpanded(false)
+    }
+  }
 
   const handleClick = () => {
     setActive(!active)
