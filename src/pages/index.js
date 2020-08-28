827104e4f619bc61f3007f1f1489a94bea3ba2c2
Brady Bridges <bradyjbridges@gmail.com>
2020-08-28 10:53:13 -0500

    Remove unecessary imports

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,6 @@
 import React from "react"
-import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import logo from "../images/logo.svg"
 
 const IndexPage = () => (
   <Layout active="home">
