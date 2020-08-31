b59b228a3e50cc74310afc7e0558069d7a722168
Brady Bridges <bradyjbridges@gmail.com>
2020-08-31 13:33:12 -0500

    Add logic to handle mobile panel, add logic to fix bug where panel expands on refresh improperly

--- src/components/panel.js
+++ src/components/panel.js
@@ -1,14 +1,41 @@
 import React, { useEffect, useRef } from "react"
+import { useWindowSize } from "./hooks"
 import "./panel.scss"
 
 export default function Panel() {
   const backPanelRef = useRef()
+  const { width } = useWindowSize()
 
   useEffect(() => {
-    window.addEventListener("scroll", handleScroll)
+    if (width < 890) {
+      backPanelRef.current.classList.remove("panel--expanded")
+      backPanelRef.current.classList.add("panel--mobile")
+    } else {
+      if (backPanelRef.current.classList.contains("panel--mobile")) {
+        backPanelRef.current.classList.remove("panel--mobile")
+      }
 
-    return () => {
-      window.removeEventListener("scroll", handleScroll)
+      if (
+        (document.body.scrollTop === 0 ||
+          document.documentElement.scrollTop === 0) &&
+        !backPanelRef.current.classList.contains("panel--expanded")
+      ) {
+        backPanelRef.current.classList.add("panel--expanded")
+      }
+
+      if (
+        (document.body.scrollTop > 0 ||
+          document.documentElement.scrollTop > 0) &&
+        backPanelRef.current.classList.contains("panel--expanded")
+      ) {
+        backPanelRef.current.classList.remove("panel--expanded")
+      }
+
+      window.addEventListener("scroll", handleScroll)
+
+      return () => {
+        window.removeEventListener("scroll", handleScroll)
+      }
     }
   })
 
