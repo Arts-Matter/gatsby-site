ddb789aa9c787c821615306590dcabb43d641c56
Brady Bridges <bradyjbridges@gmail.com>
2020-08-31 11:30:32 -0500

    Revert to using header color classes to maintain mobile styles

--- src/components/header.js
+++ src/components/header.js
@@ -17,7 +17,7 @@ const Header = ({ siteTitle, active }) => {
   const activeLinkStyles = "nav-list__item nav-list__item--active"
 
   return (
-    <header ref={headerRef} className="header magenta">
+    <header ref={headerRef} className="header header--primary">
       <div className="header-container">
         <h1 className="header-container__title">
           <Link
