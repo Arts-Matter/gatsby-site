ef364c8c13be9cd5c559d5d1d41635e2137c1088
Brady Bridges <bradyjbridges@gmail.com>
2020-09-08 17:56:41 -0500

    Refactor to avoid having to pass in repetitive elements found on pages

--- src/components/headerArea.js
+++ src/components/headerArea.js
@@ -1,11 +1,33 @@
 import React from "react"
 import "./headerArea.scss"
 
-export default function HeaderArea({ active, top, bottom }) {
+export default function HeaderArea({
+  topLeft,
+  topRight,
+  bottomTitle,
+  bottomLeft,
+  bottomRight,
+  useBottom,
+}) {
   return (
     <div className="header-area">
-      <div className="top">{top}</div>
-      {bottom && <div className="bottom">{bottom}</div>}
+      <div className="top">
+        <div className="top__left">
+          <h1 className="top__title">{topLeft}</h1>
+        </div>
+        <div className="top__right">
+          <h3 className="top__right-content">{topRight}</h3>
+        </div>
+      </div>
+      {useBottom && (
+        <div className="bottom">
+          <div className="bottom__left">
+            <span class="bottom__label">{bottomTitle}</span>
+            {bottomLeft}
+          </div>
+          <div className="bottom__right">{bottomRight}</div>
+        </div>
+      )}
     </div>
   )
 }
