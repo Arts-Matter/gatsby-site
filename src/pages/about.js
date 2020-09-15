9edf93960cadb3bda866f16fabf39c6bc821d5d1
Brady Bridges <bradyjbridges@gmail.com>
2020-09-15 17:18:14 -0500

    Add query for staff members, pass staff members to team component

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -6,6 +7,7 @@ import HeaderArea from "../components/headerArea"
 import AboutBottomHeaderArea from "../components/aboutBottomHeaderArea"
 import History from "../components/history"
 import ProgramPillars from "../components/programPillars"
+import Team from "../components/team"
 
 const title = "About"
 
@@ -14,13 +16,66 @@ const topRight =
 
 const bottom = <AboutBottomHeaderArea />
 
-const Page = () => (
-  <Layout active="about">
-    <SEO title={title} />
-    <HeaderArea topLeft="About" topRight={topRight} bottom={bottom} />
-    <History />
-    <ProgramPillars />
-  </Layout>
-)
+const Page = ({ data }) => {
+  data.allContentfulBio.nodes.splice(1, 1)
+  const teamMembers = data.allContentfulBio.nodes.reduce(
+    (members, curMember) => {
+      const { name, role } = curMember
+      const imageSrc = "https:".concat(curMember.image.fixed.src)
 
+      const descriptions = curMember.description.content.reduce(
+        (descriptions, curDescription) => {
+          descriptions.push(curDescription.content[0].value)
+          return descriptions
+        },
+        []
+      )
+
+      const newMember = {
+        name,
+        role,
+        descriptions,
+        imageSrc,
+      }
+
+      members.push(newMember)
+
+      return members
+    },
+    []
+  )
+
+  return (
+    <Layout active="about">
+      <SEO title={title} />
+      <HeaderArea topLeft="About" topRight={topRight} bottom={bottom} />
+      <History />
+      <ProgramPillars />
+      <Team team={teamMembers} />
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  {
+    allContentfulBio {
+      nodes {
+        name
+        role
+        image {
+          fixed {
+            src
+          }
+        }
+        description {
+          content {
+            content {
+              value
+            }
+          }
+        }
+      }
+    }
+  }
+`
 export default Page
