d98559f7f1d6ee6cee36419870098ef1c6a7f639
Brady Bridges <bradyjbridges@gmail.com>
2020-09-23 11:07:36 -0500

    Add relative links to programming cards

--- src/components/programmingCard.js
+++ src/components/programmingCard.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { Link } from "gatsby"
 import "./programmingCard.scss"
 
 export default function ProgrammingCard({ title, summary, id }) {
@@ -7,14 +8,13 @@ export default function ProgrammingCard({ title, summary, id }) {
     <div className="programming-card">
       <h3 className="programming-card__title">{title}</h3>
       <p className="programming-card__summary">{summary}</p>
-      <a
+      <Link
         className="programming-card__link"
-        href={href}
-        target="_blank"
-        rel="noopener noreferrer"
+        to={`/programs/${id}`}
+        aria-label="Learn More"
       >
         Learn more
-      </a>
+      </Link>
     </div>
   )
 }
