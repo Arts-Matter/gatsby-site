214c787a187d7f555daa8222b54ad22bde1d27d7
Brady Bridges <bradyjbridges@gmail.com>
2020-09-23 12:36:00 -0500

    Refactor expandable bio area to improve accessibility

--- src/components/teamMember.js
+++ src/components/teamMember.js
@@ -4,14 +4,18 @@ import "./teamMember.scss"
 export default function TeamMember({ member }) {
   const { name, role, descriptions, imageSrc } = member
   const [expanded, setExpanded] = useState(false)
-  const collapsedClasses = "collapsible-content collapsible-content--collapsed"
-  const expandedClasses = "collapsible-content collapsible-content--expanded"
   const classes = [
     "collapsible-content",
     expanded
       ? "collapsible-content--expanded"
       : "collapsible-content--collapsed",
   ]
+
+  const handleKeyDown = e => {
+    if (e.keyCode === 13) {
+      setExpanded(!expanded)
+    }
+  }
 
   return (
     <div className="team-member">
@@ -30,21 +34,24 @@ export default function TeamMember({ member }) {
       >
         <div
           className="bio-collapsible__title-container"
+          onKeyDown={handleKeyDown}
+          role="button"
+          aria-pressed={expanded}
+          aria-expanded={expanded}
+          tabIndex="0"
           onClick={() => setExpanded(!expanded)}
         >
           <div className="bio-collapsible__title-left">
             <h3 className="bio-collapsible__name">{name}</h3>
             <h4 className="bio-collapsible__role">{role}</h4>
           </div>
-          <button
+          <div
             className="bio-collapsible__expand-button"
             aria-label="Toggle Menu"
-            aria-pressed={expanded}
-            aria-expanded={expanded}
           >
             Bio
             <div className="bio-collapsible__expand-icon"></div>
-          </button>
+          </div>
         </div>
         <div className={classes.join(" ")}>
           {descriptions.map(description => (
