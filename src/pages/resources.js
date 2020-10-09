8e253aced00dbfcb2554740ca62973e7fa1b71d5
tjdunklee <tjdunklee@gmail.com>
2020-10-09 11:53:33 -0500

    Adds the top section of the resources template

--- src/pages/resources.js
+++ src/pages/resources.js
@@ -1,16 +1,18 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import HeaderArea from "../components/headerArea"
+
+import "../components/resources.scss"
 
 const title = "Resources"
+const topRight = "Lesson plans, activity guides, instructional media, student worksheets, assessment rubrics, and more provide a standards-based framework for teaching media arts in the classroom with the structure needed for students to learn."
 
 const Page = () => (
   <Layout active="resources">
     <SEO title={title} />
-    <h1>{title}</h1>
-    <Link to="/">Go back to the homepage</Link>
+    <HeaderArea topLeft={title} topRight={topRight} />
   </Layout>
 )
 
