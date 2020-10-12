b27a26f140443b07bf3fa006e5aaeda6083b2a08
Brady Bridges <bradyjbridges@gmail.com>
2020-10-12 13:29:04 -0500

    Add team bios query, format bios data

--- src/components/team.js
+++ src/components/team.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { graphql, useStaticQuery } from "gatsby"
 import "./team.scss"
 import { useWindowSize } from "./hooks"
 
@@ -6,6 +7,71 @@ import TeamMember from "./teamMember"
 
 export default function Team({ team }) {
   const { width } = useWindowSize()
+
+  const data = useStaticQuery(graphql`
+    {
+      allContentfulListOfThings(
+        filter: { listName: { eq: "About page bios" } }
+      ) {
+        totalCount
+        nodes {
+          entries {
+            ... on ContentfulBio {
+              role
+              name
+              description {
+                content {
+                  content {
+                    value
+                    marks {
+                      type
+                    }
+                  }
+                }
+              }
+              image {
+                fixed {
+                  src
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  const teamMembers = data.allContentfulListOfThings.nodes[0].entries.reduce(
+    (team, curMember) => {
+      const descriptions = curMember.description.content.reduce(
+        (descriptions, curDescription) => {
+          const desc = curDescription.content.reduce((content, curContent) => {
+            if (curContent.marks.length !== 0) {
+              content.push(curContent)
+            } else {
+              content.push(curContent.value)
+            }
+            return content
+          }, [])
+          descriptions.push(desc)
+          return descriptions
+        },
+        []
+      )
+
+      const newMember = {
+        name: curMember.name,
+        role: curMember.role,
+        imageSrc: curMember.image.fixed.src,
+        descriptions,
+      }
+
+      team.push(newMember)
+      return team
+    },
+    []
+  )
+
   return (
     <div className="team">
       <h2 className="team__title">Team</h2>
@@ -22,12 +88,12 @@ export default function Team({ team }) {
       </h3>
       <div className="team-container">
         {width > 889 ? (
-          team.map((member, i) => (
+          teamMembers.map((member, i) => (
             <TeamMember key={`${member.name}${i}`} member={member} />
           ))
         ) : (
           <div className="team-container__wrapper">
-            {team.map((member, i) => (
+            {teamMembers.map((member, i) => (
               <TeamMember key={`${member.role}${i}`} member={member} />
             ))}
           </div>
