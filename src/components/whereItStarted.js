4402c7de16d2a17720ad80ba01268d7b864e65fa
Brady Bridges <bradyjbridges@gmail.com>
2020-10-29 17:28:22 -0500

    Add structure/classes

---
+++ src/components/whereItStarted.js
@@ -0,0 +1,27 @@
+import React from "react"
+import './whereItStarted.scss'
+
+export default function WhereItStarted() {
+  return (
+    <div className="where-it-all-started">
+      <div className="where-it-all-started-container">
+        <div className="where-it-all-started__left">
+          <div className="where-it-all-started__text-container">
+            <h6>A Look Back...</h6>
+            <h4>PST: LA/LA Education Program</h4>
+            <p>
+              The PST: LA/LA Education Program engaged thousands of students and
+              teachers with creative learning opportunities through content from
+              exhibitions of Latin American and Latino art in cultural
+              institutions and museums across Southern California.
+            </p>
+            <a>
+              <button>Learn More</button>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="where-it-all-started__image"></div>
+    </div>
+  )
+}
