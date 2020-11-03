cae25b695f4663862eda6a1fb7226b1e2102a9af
Brady Bridges <bradyjbridges@gmail.com>
2020-11-02 20:55:29 -0600

    Remove typo in import

--- src/components/programsBottomHeaderArea.js
+++ src/components/programsBottomHeaderArea.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { useStaticQuery, graphql, push } from "gatsby"
+import { useStaticQuery, graphql } from "gatsby"
 import "./programsBottomHeaderArea.scss"
 import ProgrammingCard from "./programmingCard"
 
