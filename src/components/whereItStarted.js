472ec135ae7cbb00436748713ec43a5ffe19d67b
Brady Bridges <bradyjbridges@gmail.com>
2020-11-02 20:53:49 -0600

    Add functionality to move location of image based on screen width

--- src/components/whereItStarted.js
+++ src/components/whereItStarted.js
@@ -1,12 +1,17 @@
 import React from "react"
-import './whereItStarted.scss'
+import { Link } from "gatsby"
+import { useWindowSize } from "./hooks"
+import "./whereItStarted.scss"
 
 export default function WhereItStarted() {
+  const { width } = useWindowSize()
+
   return (
     <div className="where-it-all-started">
+      { width < 890 && <div className="where-it-all-started__image"></div> }
       <div className="where-it-all-started-container">
-        <div className="where-it-all-started__left">
-          <div className="where-it-all-started__text-container">
+        <div className="where-it-all-started-container__left">
+          <div className="where-it-all-started-container__text-container">
             <h6>A Look Back...</h6>
             <h4>PST: LA/LA Education Program</h4>
             <p>
@@ -15,13 +20,11 @@ export default function WhereItStarted() {
               exhibitions of Latin American and Latino art in cultural
               institutions and museums across Southern California.
             </p>
-            <a>
-              <button>Learn More</button>
-            </a>
+            <Link to="#">Learn More</Link>
           </div>
         </div>
       </div>
-      <div className="where-it-all-started__image"></div>
+      { width > 889 && <div className="where-it-all-started__image"></div> }
     </div>
   )
 }
