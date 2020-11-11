534901c4f9a7086ad3b88443f0ba5d34d6b1c3bb
Brady Bridges <bradyjbridges@gmail.com>
2020-11-11 17:30:56 -0600

    Add component for displaying a news item

---
+++ src/components/newsItem.js
@@ -0,0 +1,31 @@
+import React from "react"
+import { Link } from "gatsby"
+import "./newsItem.scss"
+
+export default function NewsItem({ image, date, title, description, id }) {
+  return (
+    <div className="news-item">
+      <div className="news-item__back-panel"></div>
+      <div className="news-item__wrapper">
+        <div className="news-item__image-container">
+          {image && (
+            <div
+              className="news-item__image"
+              style={{
+                backgroundImage: `url("${image}")`,
+              }}
+            ></div>
+          )}
+        </div>
+        {date && <h5>{date}</h5>}
+        {title && <h3>{title}</h3>}
+        <div className="news-item__description-container">
+          {description && <p>{description}</p>}
+          <Link to="/news/fill-in-later">
+            <div className="news-item__link-arrow"></div>
+          </Link>
+        </div>
+      </div>
+    </div>
+  )
+}
