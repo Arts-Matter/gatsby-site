aa730f948249a36d78209d6103a3cb82abea6d8a
Jason Zinn <jason@zinnfinity.com>
2020-11-11 20:41:59 -0600

    added bgColor prop to simplify component bgcolor

--- src/pages/news.js
+++ src/pages/news.js
@@ -7,7 +7,7 @@ import SEO from "../components/seo"
 const title = "News"
 
 const Page = () => (
-  <Layout active="news">
+  <Layout active="news" bgColor="sunflower">
     <SEO title={title} />
     <h1>{title}</h1>
     <Link to="/">Go back to the homepage</Link>
