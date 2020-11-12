1fb9a8fd5fd4fc135889c5c918c7dcd7c0d31d31
Jason Zinn <jason@zinnfinity.com>
2020-11-11 20:47:36 -0600

    added about section

---
+++ src/components/homepage/about.js
@@ -0,0 +1,10 @@
+import React from "react"
+import './about.scss'
+
+const AboutSection = () => (
+  <div className="section-about__content">
+    <p>Section About</p>
+  </div>
+)
+
+export default AboutSection
