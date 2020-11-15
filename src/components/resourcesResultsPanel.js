835a8f933804a2739bfd50cdf0f0a8c44f41a8e1
tjdunklee <tjdunklee@gmail.com>
2020-11-15 14:27:22 -0600

    Integrates all the data for the resources items.

--- src/components/resourcesResultsPanel.js
+++ src/components/resourcesResultsPanel.js
@@ -1,46 +1,93 @@
 import React from "react"
 import "./resourcesResultsPanel.scss"
 
-export default function ResourcesResultsPanel() {
-  return (
-    <div className="resources-results">
-      <div className="resource">
+const addOrdinalToGrade = (n) => {
+  if ( n === 0 ) {
+    return "Kindergarden"
+  } else {
+    const s                = ["th", "st", "nd", "rd"]
+    const v                = n % 100
+    const gradeWithOrdinal = n + (s[(v - 20) % 10] || s[v] || s[0])
 
-        <div className="resource__columns">
-
-          <div className="resource__col">
-            <div className="resource__meta-list">
-              <span class="resource__meta-item">Math</span>
-              <span class="resource__meta-item">6th Grade</span>
-              <span class="resource__meta-item resource__meta-item--strain">Photography</span>
-            </div>
-            <h2 class="resource__title">Exposing the Math in Photography</h2>
-            <div class="resource__description">
-              <p>In this unit, students will learn how math principles come into play in photography through balancing exposure to understand its effect on outcomes in photography. Students will analyze other students’ work through the Getty’s Unshuttered app to analyze how exposure and composition can affect message or meaning, and use what they learn to produce an original series of portraits of their peers to demonstrate their mastery. Finally, students will pair their photo series for display with equivalent expressions before completing a reflection on the intersection between math and photography.</p>
-            </div>
-          </div>
-
-          <div className="resource__col">
-            <div class="resource__downloads">
-              <h3 className="resource__downloads-title">Downloads</h3>
-              <ul class="resource__files-list">
-                <li className="resource__files-list-item">
-                  <a href="//assets.ctfassets.net/0yqesla6898r/61ZdODl4O1kSAlzz3OVbUJ/75d6d0991f9f4f2137626975e9e1688d/SS_Q4.pdf" download="" target="_blank" rel="noopener noreferrer">
-                    Lesson Plans
-                  </a>
-                </li>
-                <li className="resource__files-list-item">
-                  <a href="//assets.ctfassets.net/0yqesla6898r/61ZdODl4O1kSAlzz3OVbUJ/75d6d0991f9f4f2137626975e9e1688d/SS_Q4.pdf" download="" target="_blank" rel="noopener noreferrer">
-                    Lesson Plans
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-
-        </div>
-
-      </div>
-    </div>
-  )
+    return gradeWithOrdinal + " Grade"
+  }
 }
+
+const ResourcesResultsPanel = ({ resources }) => (
+  <div className="resources-results">
+
+    { resources.map(({ node }) => {
+      return (
+        <div key={node.id} className="resource">
+          <div className="resource__columns">
+
+            <div className="resource__col">
+              <div className="resource__meta-list">
+                {
+                  node.subjectArea &&
+                  <span className="resource__meta-item">
+                    { node.subjectArea.map((subject, i) => {
+                      let preText = ''
+
+                      if ( i > 0 ) {
+                        preText = ' & '
+                      }
+
+                      return <span key={i}>{ preText + subject }</span>
+                    })}
+                  </span>
+                }
+                {node.gradeLevel !== null &&
+                  <span className="resource__meta-item">{ addOrdinalToGrade( node.gradeLevel ) }</span>
+                }
+                {
+                  node.mediaArtsStrain &&
+                  <span className="resource__meta-item resource__meta-item--strain">
+                    { node.mediaArtsStrain.map((strain, i) => {
+                      let preText = ''
+
+                      if ( i > 0 ) {
+                        preText = ' & '
+                      }
+
+                      return <span key={i}>{ preText + strain }</span>
+                    })}
+                  </span>
+                }
+              </div>
+              <h2 className="resource__title">{node.title}</h2>
+              {node.description && 
+                <div className="resource__description">
+                  <p>{node.description.description}</p>
+                </div>
+              }
+            </div>
+
+            <div className="resource__col">
+              {node.resourceFiles &&
+                <div className="resource__downloads">
+                  <h3 className="resource__downloads-title">Downloads</h3>
+                  <ul className="resource__files-list">
+                    { node.resourceFiles.map((resource) => {
+                      return (
+                        <li key={resource.id} className="resource__files-list-item">
+                          <a href={resource.file.file.url} download="" target="_blank" rel="noopener noreferrer">
+                            {resource.title}
+                          </a>
+                        </li>
+                      )
+                    })}
+                  </ul>
+                </div>
+              }
+            </div>
+
+          </div>
+        </div>
+      )
+    })}
+
+  </div>
+)
+
+export default ResourcesResultsPanel
--- src/pages/resources.js
+++ src/pages/resources.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -11,13 +12,45 @@ import "../components/resources.scss"
 const title = "Resources"
 const topRight = "Lesson plans, activity guides, instructional media, student worksheets, assessment rubrics, and more provide a standards-based framework for teaching media arts in the classroom with the structure needed for students to learn."
 
-const Page = () => (
-  <Layout active="resources">
-    <SEO title={title} />
-    <HeaderArea topLeft={title} topRight={topRight} />
-    <ResourcesSearchPanel />
-    <ResourcesResultsPanel />
-  </Layout>
-)
+const Page = ({ data }) => {
+  const resources = data.allContentfulResource.edges
+
+  return (
+    <Layout active="resources">
+      <SEO title={title} />
+      <HeaderArea topLeft={title} topRight={topRight} />
+      <ResourcesSearchPanel />
+      <ResourcesResultsPanel resources={resources} />
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  {
+    allContentfulResource {
+      edges {
+        node {
+          gradeLevel
+          mediaArtsStrain
+          subjectArea
+          title
+          description {
+            description
+          }
+          id
+          resourceFiles {
+            title
+            id
+            file {
+              file {
+                url
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
 
 export default Page
