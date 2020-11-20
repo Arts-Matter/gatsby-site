62ca3e642f19fb9a59a6aa620e00bd3827954b05
Jason Zinn <jason@zinnfinity.com>
2020-11-19 21:02:08 -0600

    added size and theme to button component

--- src/components/homepage/about.js
+++ src/components/homepage/about.js
@@ -8,18 +8,20 @@ const AboutSection = () => {
   const { width } = useWindowSize()
 
   return (
-    <div className="section--about__content content-container">
-      <div className="left">
-        <h3 className="section--about__title">
-          We provide high-impact arts education programming for Los Angeles
-          schools.
-        </h3>
-        {width > 889 && <Button url="/about" text="Our Mission" />}
-      </div>
-      <div className="right">
-        <div className="color"></div>
-        <ImageGallery src1="/1.jpg" src2="/2.jpg" src3="/3.jpg" />
-        {width < 890 && <Button url="/about" text="Our Mission" />}
+    <div className="section--about__content">
+      <div class="content-container">
+        <div className="left">
+          <h3 className="section--about__title">
+            We provide high-impact arts education programming for Los Angeles
+            schools.
+          </h3>
+          {width > 889 && <Button url="/about" text="Our Mission" size="small" theme="light" />}
+        </div>
+        <div className="right">
+          <div className="color"></div>
+          <ImageGallery src1="/1.jpg" src2="/2.jpg" src3="/3.jpg" />
+          {width < 890 && <Button url="/about" text="Our Mission" size="small" theme="light" />}
+        </div>
       </div>
     </div>
   )
