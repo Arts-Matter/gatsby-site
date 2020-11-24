e0d477d0a1baaf4b137661fe56325bb74df19785
Jason Zinn <jason@zinnfinity.com>
2020-11-24 12:54:08 -0600

    replaced deprecated google fonts plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -69,17 +69,11 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      resolve: `gatsby-plugin-google-fonts`,
       options: {
         fonts: [
-          {
-            family: "IBM Plex Mono",
-            variants: [600],
-          },
-          {
-            family: "Scope One",
-            variants: [400],
-          },
+          "IBM Plex Mono\:600",
+          "Scope One\:400",
         ],
       },
     },
