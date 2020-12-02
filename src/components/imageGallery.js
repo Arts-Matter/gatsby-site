0f9ec2b2fafb9fb38e1a0e4b374f278de9622fde
Jason Zinn <jason@zinnfinity.com>
2020-12-01 21:57:58 -0600

    refactored imageGallery to use state

--- src/components/imageGallery.js
+++ src/components/imageGallery.js
@@ -1,65 +1,22 @@
-import React, { useRef } from "react"
+import React from "react"
 import "./imageGallery.scss"
 
 export default function ImageGallery({ images, controls }) {
-  const img1 = useRef()
-  const img2 = useRef()
-  const img3 = useRef()
-  const gallery = [img1, img2, img3]
-  let curImg = img1
+  const [ curIndex, setCurIndex ] = React.useState(0)
 
   const handleNextImage = isNext => {
     const newIndex = isNext ? nextImageIndex() : previousImageIndex()
-    handleStyleUpdates(newIndex)
-    curImg = gallery[newIndex]
+    setCurIndex(newIndex);
   }
 
   const previousImageIndex = () => {
-    const curIndex = gallery.findIndex(e => e === curImg)
-    let nextIndex
-
-    if (curIndex === 0) {
-      nextIndex = gallery.length - 1
-    } else {
-      nextIndex = curIndex - 1
-    }
-
+    const nextIndex = curIndex === 0 ? images.length - 1 : curIndex - 1;
     return nextIndex
   }
 
   const nextImageIndex = () => {
-    const curIndex = gallery.findIndex(e => e === curImg)
-    let nextIndex
-
-    if (curIndex === gallery.length - 1) {
-      nextIndex = 0
-    } else {
-      nextIndex = curIndex + 1
-    }
-
+    const nextIndex = curIndex === images.length - 1 ? 0 : curIndex + 1;
     return nextIndex
-  }
-
-  // TODO: Ensure only top caption displays on initial load
-
-  const handleStyleUpdates = newIndex => {
-    gallery.forEach((el, i) => {
-      if (i === newIndex) {
-        if (el.current.firstChild) {
-          el.current.firstChild.classList.remove(
-            "gallery-image__caption--no-display"
-          )
-        }
-        el.current.style.zIndex = 3
-      } else {
-        if (el.current.firstChild) {
-          el.current.firstChild.classList.add(
-            "gallery-image__caption--no-display"
-          )
-        }
-        el.current.style.zIndex = 2
-      }
-    })
   }
 
   return (
@@ -67,46 +24,25 @@ export default function ImageGallery({ images, controls }) {
       <div className="gallery">
         <div className="gallery-container">
           <div className="gallery-image-wrapper">
-            <div
-              className="gallery-image gallery-image__1"
-              ref={img1}
+            {images.map( (image, index) => {
+              return (
+              <div
+              key={index}
+              className={`gallery-image gallery-image__${index}`}
               style={{
-                backgroundImage: `url(${images[0].src})`,
+                backgroundImage: `url(${image.src})`,
+                zIndex: index === curIndex ? 3 : 2,
               }}
             >
-              {(images[0].caption || images[0].title) && (
-                <div className="gallery-image__caption">
-                  {images[0].title && (<div className="gallery-image__caption-title">{images[0].title}</div>)}
-                  {images[0].caption && (<p className="gallery-image__caption-text">{images[0].caption}</p>)}
+              {(image.caption || image.title) && (
+                <div className={index === curIndex ? 'gallery-image__caption gallery-image__caption--active' : 'gallery-image__caption'}>
+                  {image.title && (<div className="gallery-image__caption-title">{image.title}</div>)}
+                  {image.caption && (<p className="gallery-image__caption-text">{image.caption}</p>)}
                 </div>
               )}
             </div>
-            <div
-              className="gallery-image gallery-image__2"
-              ref={img2}
-              style={{ backgroundImage: `url(${images[1].src})` }}
-            >
-              {(images[1].caption || images[1].title) && (
-                <div className="gallery-image__caption">
-                  {images[1].title && (<div className="gallery-image__caption-title">{images[1].title}</div>)}
-                  {images[1].caption && (<p className="gallery-image__caption-text">{images[1].caption}</p>)}
-                </div>
-              )}
-            </div>
-            <div
-              className="gallery-image gallery-image__3"
-              ref={img3}
-              style={{
-                backgroundImage: `url("${images[2].src}")`,
-              }}
-            >
-              {(images[2].caption || images[2].title) && (
-                <div className="gallery-image__caption">
-                  {images[2].title && (<div className="gallery-image__caption-title">{images[2].title}</div>)}
-                  {images[2].caption && (<p className="gallery-image__caption-text">{images[2].caption}</p>)}
-                </div>
-              )}
-            </div>
+            )
+              })}
           </div>
           <div
             className={`gallery-controls gallery-controls--${controls.theme}`}
