906113950fdd4ee285348e337e83d377333a9bc8
Brady Bridges <bradyjbridges@gmail.com>
2020-12-14 11:14:58 -0700

    Update news item link using contentful ID

--- src/components/newsItem.js
+++ src/components/newsItem.js
@@ -2,7 +2,14 @@ import React from "react"
 import { Link } from "gatsby"
 import "./newsItem.scss"
 
-export default function NewsItem({ image, date, title, description, id, hoverEffects }) {
+export default function NewsItem({
+  image,
+  date,
+  title,
+  description,
+  id,
+  hoverEffects,
+}) {
   return (
     <div className={hoverEffects ? "news-item news-item--hover" : "news-item"}>
       <div className="news-item__back-panel"></div>
@@ -21,9 +28,11 @@ export default function NewsItem({ image, date, title, description, id, hoverEff
         {title && <h3>{title}</h3>}
         <div className="news-item__description-container">
           {description && <p>{description}</p>}
-          <Link to="/news/fill-in-later">
-            <div className="news-item__link-arrow"></div>
-          </Link>
+          {id && (
+            <Link to={`/news/${id}`}>
+              <div className="news-item__link-arrow"></div>
+            </Link>
+          )}
         </div>
       </div>
     </div>
