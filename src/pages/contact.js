05af5bf375cf9408be5ac598c1a33ce5699b64dc
Brady Bridges <bradyjbridges@gmail.com>
2020-12-17 14:22:03 -0700

    Remove unused Link import

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,12 +1,9 @@
 import React from "react"
-import { Link } from "gatsby"
-
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import HeaderArea from "../components/headerArea"
 import ContactBottomHeaderArea from "../components/contactBottomHeaderArea"
 import ContactTeam from "../components/contactTeam"
-
 
 const title = "Contact"
 const bottom = <ContactBottomHeaderArea />
