a0947adbd876ef768e297b2995570c7339120631
Brady Bridges <bradyjbridges@gmail.com>
2020-12-28 15:17:48 -0700

    Formatting

--- src/components/newsItem.js
+++ src/components/newsItem.js
@@ -13,37 +13,39 @@ export default function NewsItem({
   const renderNewsItem = () => {
     return (
       // Later on we want to use something other than the contentful ID for the link
-    <div className={hoverEffects ? "news-item news-item--hover" : "news-item"}>
-    <div className="news-item__back-panel"></div>
-    <Link className="news-item__link" to={`/news/${id}`}>
-      <div className="news-item__wrapper">
-        <div className="news-item__image-container">
-          {image && (
-            <div
-              className="news-item__image"
-              style={{
-                backgroundImage: `url("${image}")`,
-              }}
-            ></div>
-          )}
-        </div>
-        {date && <time dateTime={date}>{date}</time>}
-        {title && <h3>{title}</h3>}
-        {(description || id) && (
-          <div className="news-item__description-container">
-            {description && <p>{description}</p>}
-            {id && <div className="news-item__link-arrow"></div>}
+      <div
+        className={hoverEffects ? "news-item news-item--hover" : "news-item"}
+      >
+        <div className="news-item__back-panel"></div>
+        <Link className="news-item__link" to={`/news/${id}`}>
+          <div className="news-item__wrapper">
+            <div className="news-item__image-container">
+              {image && (
+                <div
+                  className="news-item__image"
+                  style={{
+                    backgroundImage: `url("${image}")`,
+                  }}
+                ></div>
+              )}
+            </div>
+            {date && <time dateTime={date}>{date}</time>}
+            {title && <h3>{title}</h3>}
+            {(description || id) && (
+              <div className="news-item__description-container">
+                {description && <p>{description}</p>}
+                {id && <div className="news-item__link-arrow"></div>}
+              </div>
+            )}
           </div>
-        )}
+        </Link>
       </div>
-    </Link>
-  </div>
     )
   }
 
   return (
     <React.Fragment>
-      {(id && (image || date || title || description)) && renderNewsItem()}
+      {id && (image || date || title || description) && renderNewsItem()}
     </React.Fragment>
   )
 }
