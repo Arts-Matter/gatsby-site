bde7f9ec4371fc8969018688122419a11567156d
Brady Bridges <bradyjbridges@gmail.com>
2020-12-28 14:57:45 -0700

    Update conditional rendering to allow news items with partial data to be displayed

--- src/components/newsItem.js
+++ src/components/newsItem.js
@@ -10,56 +10,40 @@ export default function NewsItem({
   id,
   hoverEffects,
 }) {
-  const renderWrapperContent = () => {
+  const renderNewsItem = () => {
     return (
-      <React.Fragment>
-        <div className="news-item__back-panel"></div>
-        <div className="news-item__wrapper">
-          <div className="news-item__image-container">
-            {image && (
-              <div
-                className="news-item__image"
-                style={{
-                  backgroundImage: `url("${image}")`,
-                }}
-              ></div>
-            )}
-          </div>
-          {date && <time dateTime={date}>{date}</time>}
-          {title && <h3>{title}</h3>}
+      // Later on we want to use something other than the contentful ID for the link
+    <div className={hoverEffects ? "news-item news-item--hover" : "news-item"}>
+    <div className="news-item__back-panel"></div>
+    <Link className="news-item__link" to={`/news/${id}`}>
+      <div className="news-item__wrapper">
+        <div className="news-item__image-container">
+          {image && (
+            <div
+              className="news-item__image"
+              style={{
+                backgroundImage: `url("${image}")`,
+              }}
+            ></div>
+          )}
+        </div>
+        {date && <time dateTime={date}>{date}</time>}
+        {title && <h3>{title}</h3>}
+        {(description || id) && (
           <div className="news-item__description-container">
             {description && <p>{description}</p>}
             {id && <div className="news-item__link-arrow"></div>}
           </div>
-        </div>
-      </React.Fragment>
+        )}
+      </div>
+    </Link>
+  </div>
     )
   }
 
   return (
-    // Later on we want to use something other than the contentful ID for the link
-    <div className={hoverEffects ? "news-item news-item--hover" : "news-item"}>
-      <div className="news-item__back-panel"></div>
-      <Link className="news-item__link" to={`/news/${id}`}>
-        <div className="news-item__wrapper">
-          <div className="news-item__image-container">
-            {image && (
-              <div
-                className="news-item__image"
-                style={{
-                  backgroundImage: `url("${image}")`,
-                }}
-              ></div>
-            )}
-          </div>
-          {date && <time dateTime={date}>{date}</time>}
-          {title && <h3>{title}</h3>}
-          <div className="news-item__description-container">
-            {description && <p>{description}</p>}
-            {id && <div className="news-item__link-arrow"></div>}
-          </div>
-        </div>
-      </Link>
-    </div>
+    <React.Fragment>
+      {(id && (image || date || title || description)) && renderNewsItem()}
+    </React.Fragment>
   )
 }
