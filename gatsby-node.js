e890b5033a1d779a00827470cb7f9a278f1678ac
Brady Bridges <bradyjbridges@gmail.com>
2021-01-06 12:36:18 -0700

    Update slugs of news article from contentful id -> title

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,11 +1,25 @@
 exports.createPages = async ({ actions, graphql }) => {
+  const convertTitleToSlug = title => {
+    if (title) {
+      // Remove anything that is not alpha numeric, "/", or " "
+      const slugNoSpecialChars = title.replace(/[^0-9a-zA-Z/\s]/g, "")
+      // Replace "/", or " ", with "-"
+      // If consecutive "-"s replace with "-"
+      return slugNoSpecialChars
+        .replace(/[/\s]/g, "-")
+        .replace(/--+/g, "-")
+        .toLowerCase()
+    }
+    return null
+  }
+
   const { data } = await graphql(`
     query {
       allContentfulNewsItem(sort: { fields: date, order: DESC }) {
         edges {
           node {
             contentful_id
-            date
+            title
           }
         }
       }
@@ -15,12 +29,13 @@ exports.createPages = async ({ actions, graphql }) => {
   // Create single news page(s)
   data.allContentfulNewsItem.edges.forEach(edge => {
     // We will want to replace this slug with something more meaningful
-    const slug = edge.node["contentful_id"]
+    const slug = convertTitleToSlug(edge.node.title)
+    const id = edge.node["contentful_id"]
 
     actions.createPage({
-      path: `news/${slug}`,
+      path: `news/${slug ? slug : id}`,
       component: require.resolve("./src/templates/singleNews.js"),
-      context: { id: slug }, // Pass ID to get news item in template
+      context: { id }, // Pass ID to get news item in template
     })
   })
 }
