c27ed717012c2332f03a82d1f1e2e89e8278d249
Brady Bridges <bradyjbridges@gmail.com>
2021-01-06 16:36:38 -0700

    Formatting

--- src/components/imageGallery.js
+++ src/components/imageGallery.js
@@ -2,20 +2,20 @@ import React from "react"
 import "./imageGallery.scss"
 
 export default function ImageGallery({ images, controls }) {
-  const [ curIndex, setCurIndex ] = React.useState(0)
+  const [curIndex, setCurIndex] = React.useState(0)
 
   const handleNextImage = isNext => {
     const newIndex = isNext ? nextImageIndex() : previousImageIndex()
-    setCurIndex(newIndex);
+    setCurIndex(newIndex)
   }
 
   const previousImageIndex = () => {
-    const nextIndex = curIndex === 0 ? images.length - 1 : curIndex - 1;
+    const nextIndex = curIndex === 0 ? images.length - 1 : curIndex - 1
     return nextIndex
   }
 
   const nextImageIndex = () => {
-    const nextIndex = curIndex === images.length - 1 ? 0 : curIndex + 1;
+    const nextIndex = curIndex === images.length - 1 ? 0 : curIndex + 1
     return nextIndex
   }
 
@@ -24,25 +24,39 @@ export default function ImageGallery({ images, controls }) {
       <div className="gallery">
         <div className="gallery-container">
           <div className="gallery-image-wrapper">
-            {images.map( (image, index) => {
+            {images.map((image, index) => {
               return (
-              <div
-              key={index}
-              className={`gallery-image gallery-image__${index}`}
-              style={{
-                backgroundImage: `url(${image.src})`,
-                zIndex: index === curIndex ? 3 : 2,
-              }}
-            >
-              {(image.caption || image.title) && (
-                <div className={index === curIndex ? 'gallery-image__caption gallery-image__caption--active' : 'gallery-image__caption'}>
-                  {image.title && (<div className="gallery-image__caption-title">{image.title}</div>)}
-                  {image.caption && (<p className="gallery-image__caption-text">{image.caption}</p>)}
+                <div
+                  key={index}
+                  className={`gallery-image gallery-image__${index}`}
+                  style={{
+                    backgroundImage: `url(${image.src})`,
+                    zIndex: index === curIndex ? 3 : 2,
+                  }}
+                >
+                  {(image.caption || image.title) && (
+                    <div
+                      className={
+                        index === curIndex
+                          ? "gallery-image__caption gallery-image__caption--active"
+                          : "gallery-image__caption"
+                      }
+                    >
+                      {image.title && (
+                        <div className="gallery-image__caption-title">
+                          {image.title}
+                        </div>
+                      )}
+                      {image.caption && (
+                        <p className="gallery-image__caption-text">
+                          {image.caption}
+                        </p>
+                      )}
+                    </div>
+                  )}
                 </div>
-              )}
-            </div>
-            )
-              })}
+              )
+            })}
           </div>
           <div
             className={`gallery-controls gallery-controls--${controls.theme}`}
