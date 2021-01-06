98536094f8b0cc7a32314badf0aca919334aa74f
Brady Bridges <bradyjbridges@gmail.com>
2021-01-06 10:08:01 -0700

    Update social bar to use links from sharing library, update props

--- src/components/socialMediaBar.js
+++ src/components/socialMediaBar.js
@@ -1,19 +1,33 @@
 import React from "react"
 import "./socialMediaBar.scss"
+import {
+  FacebookShareButton,
+  FacebookIcon,
+  TwitterShareButton,
+  TwitterIcon,
+  EmailShareButton,
+  EmailIcon,
+} from "react-share"
 
-export default function SocialMediaBar() {
+export default function SocialMediaBar({ title, url, hashtags }) {
   return (
     <div className="social-media-bar">
       <ul>
-        <a href="https://www.facebook.com">
-          <li id="fb-logo" className="social-media-bar__image"></li>
-        </a>
-        <a href="https://www.twitter.com">
-          <li id="twitter-logo" className="social-media-bar__image"></li>
-        </a>
-        <a href="https://www.gmail.com">
-          <li id="email-logo" className="social-media-bar__image"></li>
-        </a>
+        <li>
+          <FacebookShareButton url={url} quote={title}>
+            <FacebookIcon size={40} round={true} />
+          </FacebookShareButton>
+        </li>
+        <li>
+          <TwitterShareButton url={url} title={title} hashtags={hashtags}>
+            <TwitterIcon size={40} round={true} />
+          </TwitterShareButton>
+        </li>
+        <li>
+          <EmailShareButton subject={title} url={url}>
+            <EmailIcon size={40} round={true} />
+          </EmailShareButton>
+        </li>
       </ul>
     </div>
   )
