3e6fa5974c65eac1e91a4adb59f4a4cc1278a266
Brady Bridges <bradyjbridges@gmail.com>
2021-01-07 11:31:04 -0700

    Replace static quotes with contentful quotes, update logic to handle no quotes supplied

--- src/components/quotes.js
+++ src/components/quotes.js
@@ -1,44 +1,23 @@
 import React, { useState } from "react"
 import "./quotes.scss"
 
-export default function Quotes() {
+export default function Quotes({ quotes = [] }) {
   const [quoteIndex, setQuoteIndex] = useState(0)
-  const quotes = [
-    {
-      quote:
-        "The program and curriculum were easy to implement with the students. From the Professional Development to the lesson plans and video resources we could use to talk about careers in the field.”",
-      tag: "JASMINE TIGOLO | TEACHER, FOSHAY LEARNING CENTER",
-    },
-    {
-      quote:
-        "We loved working together as a class for all of us to learn more about each other and work together as one big group.”",
-      tag: "3RD GRADER, SANTA MONICA BOULEVARD CHARTER SCHOOL",
-    },
-    {
-      quote:
-        "The students really enjoyed learning about the animation process and creating their own animated clips. It was something totally new to almost all of them and they were very proud of their work.”",
-      tag: "EMILY SLASON | TEACHER, JOHN MUIR MS",
-    },
-    {
-      quote:
-        "I am grateful for the additional funding that ArtsMatter provided, which supported the resources I required for my Photography class. My colleagues and I were dismayed to realize that we had never had a PD that focused solely on the arts.”",
-      tag: "MEGHAN LEE | TEACHER, ACADEMIC LEADERSHIP COMMUNITY SCHOOL",
-    },
-    {
-      quote:
-        "There were numerous special lessons learned and for many students, this arts assignment was one of the most meaningful and special projects.”",
-      tag: "ALBERT CELIS | TEACHER, KING MS",
-    },
-  ]
 
   const handleNextQuote = () => {
+    if (quotes.length === 0) return
+
     if (quoteIndex === quotes.length - 1) return setQuoteIndex(0)
+
     const newIndex = quoteIndex + 1
     setQuoteIndex(newIndex)
   }
 
   const handlePreviousQuote = () => {
+    if (quotes.length === 0) return
+
     if (quoteIndex === 0) return setQuoteIndex(quotes.length - 1)
+
     const newIndex = quoteIndex - 1
     setQuoteIndex(newIndex)
   }
@@ -53,10 +32,10 @@ export default function Quotes() {
             <div className="quotes-container__quote-wrapper">
               <div>
                 <h2 className="quotes-container__quote">
-                  {quotes[quoteIndex].quote}
+                  {quotes[quoteIndex] && quotes[quoteIndex].quote}
                 </h2>
                 <h5 className="quotes-container__tag">
-                  {quotes[quoteIndex].tag}
+                  {quotes[quoteIndex] && quotes[quoteIndex].tag}
                 </h5>
               </div>
             </div>
