1ececfa20a105db0465c37a9eb6c7a4b8d4d1a4f
Brady Bridges <bradyjbridges@gmail.com>
2021-01-12 09:00:15 -0700

    Update learn more link to go to previewed news article

--- src/components/whereItStarted.js
+++ src/components/whereItStarted.js
@@ -1,10 +1,33 @@
 import React from "react"
+import { graphql, useStaticQuery } from "gatsby"
 import { useWindowSize } from "./hooks"
+import { convertTitleToSlug } from "./helpers"
 import "./whereItStarted.scss"
 import Button from "./button"
 
 export default function WhereItStarted() {
   const { width } = useWindowSize()
+
+  const data = useStaticQuery(graphql`
+    {
+      allContentfulNewsItem(
+        filter: {
+          title: { eq: "Pacific Standard Time: LA/LA Education Program" }
+        }
+      ) {
+        edges {
+          node {
+            date
+            title
+          }
+        }
+      }
+    }
+  `)
+
+  const {date, title} = data.allContentfulNewsItem.edges[0].node
+
+  const slug = (date && title) ? `/news/${date}/${convertTitleToSlug(title)}` : null
 
   return (
     <div className="where-it-all-started">
@@ -20,7 +43,7 @@ export default function WhereItStarted() {
               exhibitions of Latin American and Latino art in cultural
               institutions and museums across Southern California.
             </p>
-            <Button url="#" text="Learn More"/>
+            {slug && <Button url={slug} text="Learn More" />}
           </div>
         </div>
       </div>
