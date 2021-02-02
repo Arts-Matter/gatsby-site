450a244fa8a42cccd78ba6cb3610ff9b60fb88bb
Brady Bridges <bradyjbridges@gmail.com>
2021-02-02 08:57:42 -0700

    Bring in sample resource bucket, tentatively update logic to work with new data scheme

--- src/pages/resources.js
+++ src/pages/resources.js
@@ -2,7 +2,8 @@ import React, { useState } from "react"
 import { graphql } from "gatsby"
 
 // TODO
-// Update data being pulled for active resources and being passed to results panel
+// - [x] Update data being pulled for active resources and being passed to results panel
+// - [ ] Keep an eye out for contentful data structure changes
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -15,21 +16,24 @@ import "../components/resources.scss"
 
 const Page = ({ data }) => {
   const title = "Instructional Resources"
-  const topRight = "Lesson plans, activity guides, instructional media, student worksheets, assessment rubrics, and more provide a standards-based framework for teaching media arts in the classroom with the structure needed for students to learn."
-  const resources = data.allContentfulResource.edges
-  const [activeResources, setActiveResources] = useState(resources)
+  const topRight =
+    "Lesson plans, activity guides, instructional media, student worksheets, assessment rubrics, and more provide a standards-based framework for teaching media arts in the classroom with the structure needed for students to learn."
+  // const resources = data.allContentfulResource.edges
+  const newResources = data.allContentfulResourceBucket.edges
+  console.log("Resource Bucket: ", newResources)
+  const [activeResources, setActiveResources] = useState(newResources)
   const initialActiveFilters = {
-    "subjectArea": [],
-    "mediaArtsStrain": [],
-    "gradeLevel": []
+    subjectArea: [],
+    mediaArtsDiscipline: [],
+    gradeLevel: [],
   }
   const [activeFilters, setActiveFilters] = useState(initialActiveFilters)
 
   const handleFilterChange = (isFilterChecked, groupSlug, childName) => {
     let groupArray = activeFilters[groupSlug].slice()
 
-    if ( isFilterChecked ) {
-      groupArray.push(childName);
+    if (isFilterChecked) {
+      groupArray.push(childName)
     } else {
       groupArray = groupArray.filter(item => item !== childName)
     }
@@ -38,13 +42,15 @@ const Page = ({ data }) => {
     setActiveFilters(updatedActiveFilters)
 
     // set the baseline
-    let filteredResources = resources;
+    let filteredResources = newResources
 
     // filter Subject Area if any filter are set
     if (updatedActiveFilters.subjectArea.length) {
       filteredResources = filteredResources.filter(({ node }) => {
         if (node.subjectArea) {
-          return updatedActiveFilters.subjectArea.some(i => node.subjectArea.includes(i))
+          return updatedActiveFilters.subjectArea.some(i =>
+            node.subjectArea.includes(i)
+          )
         } else {
           return false
         }
@@ -52,10 +58,12 @@ const Page = ({ data }) => {
     }
 
     // filter Media Arts Strain
-    if (updatedActiveFilters.mediaArtsStrain.length) {
+    if (updatedActiveFilters.mediaArtsDiscipline.length) {
       filteredResources = filteredResources.filter(({ node }) => {
-        if (node.mediaArtsStrain) {
-          return updatedActiveFilters.mediaArtsStrain.some(i => node.mediaArtsStrain.includes(i))
+        if (node.mediaArtsDiscipline) {
+          return updatedActiveFilters.mediaArtsDiscipline.some(i =>
+            node.mediaArtsDiscipline.includes(i)
+          )
         } else {
           return false
         }
@@ -66,7 +74,9 @@ const Page = ({ data }) => {
     if (updatedActiveFilters.gradeLevel.length) {
       filteredResources = filteredResources.filter(({ node }) => {
         if (node.gradeLevel !== null) {
-          return updatedActiveFilters.gradeLevel.some(i => node.gradeLevel === parseInt(i))
+          return updatedActiveFilters.gradeLevel.some(
+            i => node.gradeLevel === parseInt(i)
+          )
         } else {
           return false
         }
@@ -76,50 +86,95 @@ const Page = ({ data }) => {
     setActiveResources(filteredResources)
   }
 
-  const handleSearch = (searchTerm) => {
-    const filterCheckboxes = document.querySelectorAll('.fancy-checkbox input')
-    
-    // Clear all the checkboxes because they aren't used for search
-    filterCheckboxes.forEach((filter) => {
-      filter.checked = false
-    })
+  // Not used after removing search by standard
 
-    // Clear active filter state
-    setActiveFilters(initialActiveFilters)
+  // const handleSearch = searchTerm => {
+  //   const filterCheckboxes = document.querySelectorAll(".fancy-checkbox input")
 
-    // Bail and reset resources if search term is blank
-    if (!searchTerm) {
-      setActiveResources(resources)
-      return
-    }
+  //   // Clear all the checkboxes because they aren't used for search
+  //   filterCheckboxes.forEach(filter => {
+  //     filter.checked = false
+  //   })
 
-    // Show resources that match search term
-    let searchedResources = resources
-    
-    searchedResources = searchedResources.filter(({ node }) => {
-      if (node.standard) {
-        const standardArray = node.standard.split(",").map(item => item.trim());
-        return standardArray.includes(searchTerm)
-      } else {
-        return false
-      }
-    })
+  //   // Clear active filter state
+  //   setActiveFilters(initialActiveFilters)
 
-    setActiveResources(searchedResources)
-  }
-  
-  return (  
+  //   // Bail and reset resources if search term is blank
+  //   if (!searchTerm) {
+  //     setActiveResources(newResources)
+  //     return
+  //   }
+
+  //   // Show resources that match search term
+  //   let searchedResources = newResources
+
+  //   searchedResources = searchedResources.filter(({ node }) => {
+  //     if (node.standard) {
+  //       const standardArray = node.standard.split(",").map(item => item.trim())
+  //       return standardArray.includes(searchTerm)
+  //     } else {
+  //       return false
+  //     }
+  //   })
+
+  //   setActiveResources(searchedResources)
+  // }
+
+  return (
     <Layout active="resources" bgColor="geo">
       <SEO title={title} />
       <HeaderArea topLeft={title} topRight={topRight} />
-      <ResourcesSearchPanel handleFilterChange={handleFilterChange} handleSearch={handleSearch} />
-      {activeResources.length > 0
-        ? <ResourcesResultsPanel resources={activeResources} />
-        : <ResourcesNotFound />
-      }
+      <ResourcesSearchPanel handleFilterChange={handleFilterChange} />
+      {activeResources.length > 0 ? (
+        <ResourcesResultsPanel resources={activeResources} />
+      ) : (
+        <ResourcesNotFound />
+      )}
     </Layout>
   )
 }
+
+// export const query = graphql`
+//   {
+//     allContentfulResource {
+//       edges {
+//         node {
+//           gradeLevel
+//           mediaArtsStrain
+//           subjectArea
+//           standard
+//           title
+//           description {
+//             description
+//           }
+//           id
+//           resourceFiles {
+//             title
+//             id
+//             file {
+//               file {
+//                 url
+//               }
+//             }
+//           }
+//         }
+//       }
+//     }
+//   }
+// `
+
+/**
+ * New query to pull from the Resource Bucket content type
+ *
+ * Note:
+ *
+ * - Standard has been removed entirely
+ * - mediaArtsStrain is updated to mediaArtsDiscipline
+ * - We will want to use the title to create a slug, or maybe offer them a slug field for this content type as well (like we will do for news)
+ * - Classroom photos has a max of 3 images
+ * - studentArtwork and instructional resources, we will want to confirm the file type
+ * - videos is a full link right now, but this is just a text field. You will want to see how YouTube embed plugins prefer this string to come and we can update accordingly.
+ */
 
 export const query = graphql`
   {
@@ -147,24 +202,6 @@ export const query = graphql`
         }
       }
     }
-  }
-`
-
-/**
- * New query to pull from the Resource Bucket content type
- * 
- * Note:
- * 
- * - Standard has been removed entirely
- * - mediaArtsStrain is updated to mediaArtsDiscipline
- * - We will want to use the title to create a slug, or maybe offer them a slug field for this content type as well (like we will do for news)
- * - Classroom photos has a max of 3 images
- * - studentArtwork and instructional resources, we will want to confirm the file type
- * - videos is a full link right now, but this is just a text field. You will want to see how YouTube embed plugins prefer this string to come and we can update accordingly.
- */
-/*
-export const query = graphql`
-  {
     allContentfulResourceBucket {
       edges {
         node {
@@ -181,23 +218,28 @@ export const query = graphql`
             file {
               fileName
               contentType
+              url
             }
             title
           }
           featuredImage {
             file {
               fileName
+              url
             }
           }
           classroomPhotos {
             file {
-              fileName
+              url
             }
+            description
+            title
           }
           studentArtwork {
             file {
               contentType
               fileName
+              url
             }
           }
         }
@@ -205,6 +247,5 @@ export const query = graphql`
     }
   }
 `
-*/
 
 export default Page
